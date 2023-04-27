76c9d3de18f9046e01b3653db23fbf67e6aafa7f
harshitvangara <harshitvangara@gmail.com>
2023-04-27 21:04:36 +0530

    UI changes

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -1,43 +1,20 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+//import MenuIcon from '@mui/icons-material/Menu';
 import { useAuth0 } from "@auth0/auth0-react";
-import {  Group, Menu, UnstyledButton, createStyles} from "@mantine/core";
-import { useState } from "react";
-const useStyles = createStyles((theme) => ({
-    user: {
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-      borderRadius: theme.radius.sm,
-      transition: 'background-color 100ms ease'
-}}))
+export  function Logout() {
+  let {user,logout,} = useAuth0();
+  const logoutWithRedirect = () =>logout({returnTo: window.location.origin });
+  
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Button color="inherit" onClick={() => logoutWithRedirect()} sx={{marginLeft:"1100px",textAlign:"end"}}>Logout</Button>
+    </Box>
+  );
+}
 
-  
-export const  Logout = () => {
-    const { classes, cx } = useStyles();
-    const [userMenuOpened, setUserMenuOpened] = useState(false);
-    let {user,logout,} = useAuth0();
-    const logoutWithRedirect = () =>logout({returnTo: window.location.origin });
-  
-    return(
-        <div>
-        <Menu
-        width={76}
-        position="bottom-end"
-        transition="pop-top-right"
-        onClose={() => setUserMenuOpened(false)}
-        onOpen={() => setUserMenuOpened(true)}
-      >
-        <Menu.Target>
-          <UnstyledButton
-            className={cx(classes.user)}
-          >
-            <Group spacing={5} text-align="end">
-                {user.name} 
-            </Group>
-          </UnstyledButton>
-        </Menu.Target>
-        <Menu.Dropdown>
-          <Menu.Item onClick={() => logoutWithRedirect()} sx={{textAlign:"end"}}>Logout</Menu.Item>
-        </Menu.Dropdown>
-      </Menu>
-      </div>
-    )
-};
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,39 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import {  Header,Group,Button,Box,} from '@mantine/core';
-import { Logout } from './logout';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import { useAuth0 } from "@auth0/auth0-react";
-  
-  export function Navbar() {
-    const {isAuthenticated, loginWithRedirect} = useAuth0();
-  
-    return (
-     
-        <Header height={60} px="md">
-          <Group position="apart" sx={{ height: '100%' }}>
-            <Box></Box>
-            <Group>
-              {!isAuthenticated && <Button onClick={() => loginWithRedirect({})} variant="default">Log in</Button>}
+import { Logout } from './logout';
 
-              {isAuthenticated && <Logout/> }
-            </Group>
-  
-          
-          </Group>
-        </Header>
-      
-    );
-  }
+
+export  function Navbar() {
+
+
+  const {isAuthenticated, loginWithRedirect} = useAuth0();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            
+          </Typography>
+         {!isAuthenticated &&<Button color="inherit" onClick={() => loginWithRedirect({})}>Login</Button>}
+
+         {isAuthenticated && <Logout/> }
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
